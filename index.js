1b24303c8e40468f19e471ac839e83759b0f5f61
Jacob Sánchez <jacobszpz@protonmail.com>
2024-09-05 22:22:53 +0100

    Remove unnecessary line

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const countries = require("i18n-iso-countries");
 try {
   const name = core.getInput('name').toLowerCase();
   const name = core.getInput('locale').toLowerCase();
-  const country = countries[name];
 
   const iso2 = countries.getAlpha2Code(name, locale);
   const iso3 = countries.getAlpha3Code(name, locale);
